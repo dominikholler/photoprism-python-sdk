c2f98fd47926b081b49cefd9cadd67df9957964a
Dominik Holler <dominik.holler@gmx.net>
2021-05-08 17:25:17 +0200

    more scalable

--- examples/create_albums_from_tags.py
+++ examples/create_albums_from_tags.py
@@ -2,6 +2,7 @@
 import photoprism
 
 # api = photoprism.client.Client()
+
 api = photoprism.Client(
     domain='http://phototest:2342',
     username='admin',
@@ -9,17 +10,30 @@ api = photoprism.Client(
     debug=False
 )
 
-
 albums = {album['Title']: album for album in api.get_albums(type='album')}
 
-for photo in api.get_photos():
-    info = api.get_photo(photo['UID'])
-    subject = info['Details']['Subject']
-    if len(subject):
-        for tag in subject.split(', '):
-            if tag not in albums.keys():
-                albums[tag] = api.create_album(tag)
-            api.add_photo_to_album(
-                album_uid=albums[tag]['UID'],
-                photo_uid=photo['UID']
-            )
+
+offset = 0
+count = 0
+empty = False
+
+while not empty:
+    photos = api.get_photos(offset=offset)
+    count = len(photos)
+    empty = count == 0
+    print(f'offset: {offset}, count:{count}, empty:{empty}')
+    offset = offset + count
+
+    for photo in photos:
+        info = api.get_photo(photo['UID'])
+        print(info['Path'] + '/' + info['Name'])
+        subject = info['Details']['Subject']
+        if len(subject):
+            for tag in subject.split(', '):
+                if tag not in albums.keys():
+                    albums[tag] = api.create_album(tag)
+                api.add_photo_to_album(
+                    album_uid=albums[tag]['UID'],
+                    photo_uid=photo['UID']
+                )
+
